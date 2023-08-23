9804d9e425bc660c98c1b671e18153f7bef80903
moshoaib21 <mohammadshoaibkhan900@gmail.com>
2023-08-23 22:16:59 +0530

    job-description/applied/

--- login/Student/urls.py
+++ login/Student/urls.py
@@ -1,9 +1,17 @@
 
 from django.urls import path
 from .views import *
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path("home",HomePage,name='home'),
-    path("viewprofile",ViewProfile,name='viewprofile'),
-]
+    path("", HomePage, name='home'),
+    path("viewprofile", ViewProfile, name='viewprofile'),
+    path("personal-info", create_personal_info, name="personal-info"),
+    path("job-description/<int:id>", Job_Description, name="job-description"),
+    path("interest/<int:job_id>", job_application, name="interest"),
+    # path("upload-resume", Upload_Resume, name="upload-resume"),
+    # path("download/<int:id>", Download_Resume, name='download')
+    path('job-search', Job_Search, name='job-search'),
+] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
--- login/app/models.py
+++ login/app/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class JobPosting(models.Model):
     job_title = models.CharField(max_length=100, blank=True, null=True)
     company_name = models.CharField(max_length=100, blank=True, null=True)
@@ -19,8 +20,5 @@ class JobPosting(models.Model):
 
     
 
-
-    
     def __str__(self):
         return f"{self.job_title} at {self.company_name}"
-
